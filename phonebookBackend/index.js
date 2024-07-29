f6072699990b4af909eae491df14f1908d945999
Anon2148 <privext@tutanota.com>
2024-07-29 16:40:05 +0200

    feat: exercise 3.9, phonebook backend step 9 done

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -4,6 +4,8 @@ const morgan = require("morgan");
 
 app.use(express.json());
 
+app.use(express.static("dist"));
+
 app.use(
   morgan("tiny", {
     skip: function (req, res) {
@@ -118,7 +120,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end();
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
