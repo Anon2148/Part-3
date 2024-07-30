e5fd55bfa3ccf006e0a766cc5581b1f268599aac
Anon2148 <privext@tutanota.com>
2024-07-30 17:23:56 +0200

    feat: exercise 3.15, Phonebook backend step 3 done

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -69,12 +69,15 @@ app.get("/api/persons", (request, response) => {
 app.get("/api/persons/:id", (request, response) => {
   Person.findById(request.params.id)
     .then((person) => {
-      response.json(person);
+      if (person) {
+        response.json(person);
+      } else {
+        response.status(404).end();
+      }
     })
     .catch((error) => {
-      response.status(404).json({
-        error: "person not found",
-      });
+      console.log(error);
+      response.status(400).send({ error: "malformatted id" });
     });
 });
 
